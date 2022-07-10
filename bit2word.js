fcb1e9b63b1f0215321842b83892656dc9235482
Denis Nobre <denis@denisnobre.com>
2022-07-10 11:03:10 -0300

    add topic

--- bit2word.js
+++ bit2word.js
@@ -5,13 +5,23 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             var ar = msg.payload;
             var s = 0;
+            var vInt = [];
+            var vBool = [];
+            for(var i in ar){
+                vInt.push(Number(ar[i]));
+                vBool.push(Boolean(ar[i]));
+            };
             for(var x = 0; x < ar.length; x++){
                 var j = 2 ** x;
                 if(ar[x]){
-                    s+=j
-                }
-            }
+                    s+=j;
+                };
+            };
             msg.payload = s;
+            msg.topic = {
+                "vBool": vBool,
+                "vInt": vInt
+            };
             node.send(msg);
         });
     }
